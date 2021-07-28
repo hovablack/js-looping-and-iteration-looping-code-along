7f74f03644fd5120ba41e038d13e198ecfa3b771
Hova Black <nevermind92@hotmail.com>
2021-07-28 07:31:30 -0500

    Tests passing

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(arrName, eventName) {
+  let newArr = [];
+  for (let i = 0; i < arrName.length; i++) {
+    newArr.push(`Thank you, ${arrName[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return newArr
+}
+
+function countDown(x) {
+  while (x > 0) {
+    console.log(x--);
+  }
+  console.log(x);
+}
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+//   return gifts;
+// }
